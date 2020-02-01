a7d47f2766d600307d2cf080c35390313a8c9163
Farid El-Aouadi <faridelaouadi@hotmail.co.uk>
2020-02-01 13:51:49 +0000

    added graph to show prediction

--- regression.py
+++ regression.py
@@ -8,6 +8,11 @@ from sklearn import preprocessing, svm, model_selection
 #preprocessing is used for feature scaling
 #cross validation is used to split the dataset intp testing and training
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+import datetime
+
+style.use('ggplot')
 
 df = quandl.get("WIKI/GOOGL") #getting the data for the google stock
 
@@ -23,15 +28,44 @@ df.fillna(value=-99999, inplace=True) #this is how we will handle missing data
 forecast_out = int(math.ceil(0.01 * len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
+
+
+X = np.array(df.drop(['label'], 1))
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
 df.dropna(inplace=True)
 
-X = np.array(df.drop(['label'], 1)) #our features are everything except label
 y = np.array(df['label'])
 
-X = preprocessing.scale(X) #feature scaling on our features
-
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2) #20 percent of data is the test data
-clf = LinearRegression()
+clf = LinearRegression(n_jobs=-1) #n jobs is the number of jobs it can run in parallel, -1 means run as many as you can
 clf.fit(X_train, y_train)
 confidence = clf.score(X_test, y_test)
-print(confidence)
+
+
+forecast_set = clf.predict(X_lately) #making predictions for 30 days
+print(forecast_set, confidence, forecast_out)
+
+
+
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += 86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
